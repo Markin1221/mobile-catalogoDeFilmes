3864f1d7ac1877c57d9213e25fff6de685714133
gustavotorresdv <ggustavolopes1@gmail.com>
2025-11-08 18:14:41 -0300

    Rodando e fazendo testes
    tentei colocar imagem de fundo mas nao deu certo, fiz a rota para page2 no nome de Page2 no toolbar

--- receita/src/app/services/receitas-api.spec.ts
+++ receita/src/app/services/receitas-api.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';  // Necessário para testar HttpClient
+import { HttpClientTestingModule } from '@angular/common/http/testing';  
 
 import { ApiService } from './receitas-api';
 
