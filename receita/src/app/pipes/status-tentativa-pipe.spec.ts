87be4582013afbc5f49dcfd86e3167294aa21cdd
gustavotorresdv <ggustavolopes1@gmail.com>
2025-11-12 18:02:05 -0300

    Pipe status
    criei uma pipe personalizada que diz o status em que a pessoa está (derrota, vitoria, tentando)

---
+++ receita/src/app/pipes/status-tentativa-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { StatusTentativaPipe } from './status-tentativa-pipe';
+
+describe('StatusTentativaPipe', () => {
+  it('create an instance', () => {
+    const pipe = new StatusTentativaPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
